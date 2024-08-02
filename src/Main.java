0b1e2802d540910ef69cdda61e6cd59be249f6f8
Augeni <alina28.4@yandex.ru>
2024-08-02 13:45:43 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задание №1
+        Tasks.printThreeWords();
+        //Задание №2
+        Tasks.checkSumSign();
+        //Задание №3
+        Tasks.printColor();
+        //Задание №4
+        Tasks.compareNumbers();
+        //Задание №5
+        Tasks.checkNumSum(4,17);
+        //Задание №6
+        Tasks.isNumPosOrNeg(-4);
+        //Задание №7
+        Tasks.isNumNegative(2);
+        //Задание №8
+        Tasks.printString("Test ", 7);
+        //Задание №9
+        Tasks.checkYear(1604);
+        //Задание №10
+        Tasks.task10();
+        //Задание №11
+        Tasks.task11();
+        //Задание №12
+        Tasks.task12();
+        //Задание №13
+        Tasks.task13();
+        //Задание №14
+        Tasks.task14(6, 435);
+    }
+}
